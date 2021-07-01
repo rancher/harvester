f227cb58a983857bc39b09257d169a6d37b3ace4
Weihang Lo <weihang.lo@suse.com>
2021-07-01 17:26:34 +0800

    Add schemas of new actions for hotplug volumes
    - New action `addvolume`
    - New action `removevolume`
    - New action input `AddVolumeInput`
    - New action input `RemoveVolumeInput`
    
    Signed-off-by: Weihang Lo <weihang.lo@suse.com>

--- pkg/api/vm/schema.go
+++ pkg/api/vm/schema.go
@@ -33,6 +33,8 @@ func RegisterSchema(scaled *config.Scaled, server *server.Server, options config
 	server.BaseSchemas.MustImportAndCustomize(RestoreInput{}, nil)
 	server.BaseSchemas.MustImportAndCustomize(MigrateInput{}, nil)
 	server.BaseSchemas.MustImportAndCustomize(CreateTemplateInput{}, nil)
+	server.BaseSchemas.MustImportAndCustomize(AddVolumeInput{}, nil)
+	server.BaseSchemas.MustImportAndCustomize(RemoveVolumeInput{}, nil)
 
 	vms := scaled.VirtFactory.Kubevirt().V1().VirtualMachine()
 	vmis := scaled.VirtFactory.Kubevirt().V1().VirtualMachineInstance()
@@ -102,6 +104,8 @@ func RegisterSchema(scaled *config.Scaled, server *server.Server, options config
 				backupVM:       &actionHandler,
 				restoreVM:      &actionHandler,
 				createTemplate: &actionHandler,
+				addVolume:      &actionHandler,
+				removeVolume:   &actionHandler,
 			}
 			apiSchema.ResourceActions = map[string]schemas.Action{
 				startVM:   {},
@@ -124,6 +128,12 @@ func RegisterSchema(scaled *config.Scaled, server *server.Server, options config
 				},
 				createTemplate: {
 					Input: "createTemplateInput",
+				},
+				addVolume: {
+					Input: "addVolumeInput",
+				},
+				removeVolume: {
+					Input: "removeVolumeInput",
 				},
 			}
 		},
